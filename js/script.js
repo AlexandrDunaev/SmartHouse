5586de4a7f8064738f104e8c84405b5e9b00f914
Alexandr Dunaev <ghost74rus@gmail.com>
2022-05-22 19:41:12 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+   document.querySelectorAll('.tabs-nav__btn').forEach(function (tabsBtn) {
+      tabsBtn.addEventListener('click', function (event) {
+         const path = event.currentTarget.dataset.path
+
+         document.querySelectorAll(".tabs-item").forEach(function (tabContent) {
+            tabContent.classList.remove("tabs-item--active")
+         })
+         document.querySelector(`[data-target="${path}"]`).classList.add('tabs-item--active')
+      })
+   })
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+   document.querySelectorAll('.scenarios-nav__btn').forEach(function (tabsBtn) {
+      tabsBtn.addEventListener('click', function (event) {
+         const path = event.currentTarget.dataset.path
+
+         document.querySelectorAll(".scenarios-item").forEach(function (tabContent) {
+            tabContent.classList.remove("scenarios-item--active")
+         })
+         document.querySelector(`[data-target="${path}"]`).classList.add('scenarios-item--active')
+      })
+   })
+});
+
+const mySwiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.howwework-btn-next',
+      prevEl: '.howwework-btn-prev',
+   },
+});
+
+const eqSwiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.equipment__swiper-button-next',
+      prevEl: '.equipment__swiper-button-prev',
+   },
+
+   pagination: {
+      el: '.equipment__swiper-pagination',
+      type: 'bullets',
+   },
+});
